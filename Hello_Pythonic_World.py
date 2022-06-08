8cd1e1224c3e3766123995d5573c63fcfb3b1d43
stmark707 <stmark707@gmail.com>
2022-06-07 20:34:48 -0600

    Completed hello app with Michael

---
+++ Hello_Pythonic_World.py
@@ -0,0 +1,10 @@
+print("--------------------------------------------") # can use single quotes or double quotes in python
+print("                Hello App               ")
+print("--------------------------------------------")
+print("")
+
+
+user_text = input('What is your name? ') #Can just type input in python no need to make a system call like Java
+greeting =  'Nice to meet you ' + user_text
+print(greeting) 
+
